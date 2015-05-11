caf6cd5ab59f3c5e4011d058f5c4cdcd5cdbdfd9
anderson-arg <anderson.araujo.g@gmail.com>
2015-05-10 23:00:04 -0300

    add comentario

--- Game Cliente-Servidor/src/app/Main.java
+++ Game Cliente-Servidor/src/app/Main.java
@@ -6,6 +6,7 @@ import frame.Game;
 
 public class Main {
 	
+	// Main da classe Game.java
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
